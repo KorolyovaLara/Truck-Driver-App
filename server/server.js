fa0c4b539a95fdd06f51d1b1631be379d2e3dc88
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-08-11 23:06:06 +1000

    attept 2 to fix Heroku GET problem

--- server/server.js
+++ server/server.js
@@ -17,16 +17,16 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+	app.use(express.static(path.join(__dirname, "client", "build")));
 }
+
+app.get("*", (_, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
