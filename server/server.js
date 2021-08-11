04fd2c5a1c68f388f437d3e3f98da480a4bc18ae
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-08-11 23:30:51 +1000

    attept 5 to fix Heroku GET problem

--- server/server.js
+++ server/server.js
@@ -21,14 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
+  app.use(express.static(resolve(process.cwd(), 'client/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    res.sendFile(resolve(process.cwd(), 'client/build/index.html'))
+  })
 }
 
 db.once("open", () => {
